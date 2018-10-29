328d6b0f0d2d5ea0694b754fcc53959e63b592f2
Manuel Ernst <manuel.ernst@methodpark.de>
2018-10-29 11:56:50 +0100

    add comments

--- src/index.js
+++ src/index.js
@@ -5,8 +5,10 @@ import Thermometer from './Thermometer';
 
 const root = document.getElementById('root');
 
+// fake temperature value, gets recaclulated every second
 let temp = 20;
 function renderThermometer() {
+  // rerender the thermometer with an updated temperature value
   ReactDOM.render(<Thermometer temp={temp} />, root);
 
   temp = temp + (Math.random() * (Math.random() < 0.5 ? 1 : -1))
